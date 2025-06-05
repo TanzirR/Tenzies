b4831f66635c87466f70a034d4df7ab514107cbe
Tanzir Razzaque <tanzir.razzaque@gmail.com>
2025-06-05 22:43:45 +0600

    Dice changes color on clicking
    hold func added which toggles the color of the dice based on its id.

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,56 @@
-import { useState } from "react"
-import Die from "./Die.jsx"
+import { useState } from "react";
+import { nanoid } from "nanoid";
+import Die from "./Die.jsx";
 
+export default function App() {
+  const [dice, setDice] = useState(generateAllNewDice());
 
-export default function App(){
-
-  const [values, setValues] = useState(generateAllNewDice()) 
-
-  const diceElements = values.map(function(value){
-    return (<Die value =  {value}/>)
-  })
-
-  //Generate an array of 10 numbers between 1 and 6 inclusive 
+  //Generate an array of 10 numbers between 1 and 6 inclusive
   function generateAllNewDice() {
-        const newDice = []
-        for (let i = 0; i < 10; i++) {
-            const randNum = Math.ceil(Math.random() * 6)
-            newDice.push(randNum)
-        }
-        return newDice
+    // newDice is an array of objs: { value: <random number>, isHeld: false }
+    const newDice = [];
+    for (let i = 0; i < 10; i++) {
+      const randNum = Math.ceil(Math.random() * 6);
+      newDice.push({ id: nanoid(), value: randNum, isHeld: false });
+    }
+    return newDice;
   }
+  //When a dice is held, isHeld is toggled
+  function hold(id) {
+    setDice((prevDice) => {
+      return prevDice.map((prev) => {
+        if (id === prev.id) {
+          return { ...prev, isHeld: !prev.isHeld };
+        } else {
+          return prev;
+        }
+      });
+    });
+  }
+  //Map over the newDice to display the 10 die
+  const diceElements = dice.map(function (diceObj) {
+    return (
+      <Die
+        key={diceObj.id}
+        value={diceObj.value}
+        isHeld={diceObj.isHeld}
+        hold={() => hold(diceObj.id)}
+      />
+    );
+  });
 
-  //Update the state of the values 
-  function rollDice(){
-    setValues(generateAllNewDice())
+  //Update the state of the values
+  function rollDice() {
+    setDice(generateAllNewDice());
   }
 
   return (
     <main>
-      <div className = "dice-container">
-        {diceElements}
-      </div>
-      
-      <button className="roll-btn" onClick={rollDice}>Roll</button>
+      <div className="dice-container">{diceElements}</div>
 
+      <button className="roll-btn" onClick={rollDice}>
+        Roll
+      </button>
     </main>
-    
-  )
+  );
 }
--- src/Die.jsx
+++ src/Die.jsx
@@ -1,5 +1,10 @@
-export default function Die(props){
-    return (
-        <button>{props.value}</button>
-    )
+export default function Die(props) {
+  const styles = {
+    backgroundColor: props.isHeld ? "#59E391" : "white",
+  };
+  return (
+    <button style={styles} onClick={props.hold}>
+      {props.value}
+    </button>
+  );
 }
